80d7129ddf2e81e7492057ae072189683dc195a5
halas98 <132351477+halas98@users.noreply.github.com>
2023-08-01 13:40:45 +0100

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the first node in the list
  *
  * Return: pointer to the first node in the new list
- */
+*/
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
