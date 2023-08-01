61302120171b089de30dbef4992e0c08df22984f
halas98 <132351477+halas98@users.noreply.github.com>
2023-08-01 12:14:45 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,8 +8,9 @@
  * @idx: Index where the new node should be added (starting from 0)
  * @n: Value to be set in the new node
  *
- * Returns: 1 if it succeeded, -1 if it failed
- */
+ * Return: The address of the new node, or NULL if it failed
+*/
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
     listint_t *new_node, *current;
